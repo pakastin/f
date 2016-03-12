11c4e6814d9dd941446bebbbadcbec1e30561ac1
Juha Lindstedt <juha@pakastin.fi>
2016-03-12 15:47:27 +0200

    small fix..

--- src/svg.js
+++ src/svg.js
@@ -2,7 +2,7 @@
 import { mount } from './index';
 
 export function svg (tagName, attrs) {
-  var element = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  var element = document.createElementNS('http://www.w3.org/2000/svg', tagName);
 
   for (var attr in attrs) {
     element[attr] = attrs[attr];
