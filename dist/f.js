172d118a319ff4f71ac95ae7f65af637396b767b
Juha Lindstedt <juha@pakastin.fi>
2016-03-14 22:57:45 +0200

    added List.update callback & postponed remove middleware

--- dist/f.js
+++ dist/f.js
@@ -45,17 +45,27 @@
     this.initData = initData;
   }
 
-  List.prototype.update = function (data) {
+  List.prototype.update = function (data, cb) {
     var View = this.View;
     var key = this.key;
     var lookup = this.lookup;
     var newLookup = {};
     var views = this.views = new Array(data.length);
+    var added = [];
+    var updated = [];
+    var removed = [];
 
     for (var i = 0; i < data.length; i++) {
       var item = data[i];
       var id = key != null ? item[key] : i;
-      var view = lookup[id] || new View(this.initData, item);
+      var view = lookup[id];
+
+      if (!view) {
+        view = new View(this.initData, item);
+        added[added.length] = view;
+      } else {
+        updated[updated.length] = view;
+      }
 
       view.update && view.update(item);
       views[i] = view;
@@ -65,11 +75,12 @@
 
     for (var id in lookup) {
       if (!newLookup[id]) {
-        if (lookup[id].remove) {
-          lookup[id].el.removing = true;
-          scheduleRemove(this.parent, lookup[id]);
+        var view = lookup[id];
+        removed[removed.length] = view;
+        if (view.remove) {
+          view.el.removing = true;
         } else {
-          this.parent && unmount(this.parent, lookup[id]);
+          this.parent && unmount(this.parent, view);
         }
       }
     }
@@ -86,6 +97,19 @@
     }
 
     this.lookup = newLookup;
+
+    cb && cb({
+      added: added,
+      updated: updated,
+      removed: removed
+    });
+
+    for (var i = 0; i < removed.length; i++) {
+      var view = removed[i];
+      scheduleRemove(this.parent, view);
+    }
+
+    return this;
   }
 
   function scheduleRemove (parent, child) {
